c43dffb54b4d0fd84ce9969a6c3a60af43f733b2
kochuradmytro <kochura.dmytro@student.sumdu.edu.ua>
2026-03-24 17:32:50 +0200

    Add Clothes class

---
+++ src/main/java/Clothes.java
@@ -0,0 +1,45 @@
+﻿public class Clothes {
+    private int id;
+    private String name;
+    private String size;
+    private double price;
+
+    public Clothes(int id, String name, String size, double price) {
+        this.id = id;
+        this.name = name;
+        this.size = size;
+        this.price = price;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}

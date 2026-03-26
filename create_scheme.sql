32e92540888e9305148ca40276907e303809148e
kochuradmytro <kochura.dmytro@student.sumdu.edu.ua>
2026-03-26 13:41:04 +0200

    Add db settings

---
+++ create_scheme.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE clothes_store (
+   id INTEGER PRIMARY KEY,
+   type VARCHAR(50) NOT NULL,
+   name VARCHAR(255) NOT NULL,
+   size VARCHAR(50) NOT NULL,
+   price DOUBLE PRECISION NOT NULL,
+   quantity INTEGER NOT NULL,
+   material VARCHAR(255),
+   sleeve_type VARCHAR(255),
+   fit_type VARCHAR(255),
+   ripped BOOLEAN,
+   print_type VARCHAR(255),
+   sports_style BOOLEAN
+);
